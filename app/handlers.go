9ef46f940a7630b5e0e641d3499db0f9089b5cc1
Belaid Alahian <alahian.belaid@gmail.com>
2020-02-22 18:15:11 +0100

    prodrams page

--- app/handlers.go
+++ app/handlers.go
@@ -7,3 +7,8 @@ import (
 func (a *App) HandleHome(w http.ResponseWriter, r *http.Request) {
 	a.html(w, "home.page", M{})
 }
+
+//HandlePrograms is good for your life
+func (a *App) HandlePrograms(w http.ResponseWriter, r *http.Request) {
+	a.html(w, "programs.page", M{})
+}
--- app/routes.go
+++ app/routes.go
@@ -15,6 +15,7 @@ func (a *App) routes() *chi.Mux {
 
 	r.Get("/static/*", Static(os.Getenv("STATIC_PATH")))
 	r.Get("/", a.HandleHome)
+	r.Get("/programs", a.HandlePrograms)
 
 	return r
 }
