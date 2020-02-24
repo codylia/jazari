5c23f36aff770baa6dc72a852d09f6d5e5bc52c7
Belaid Alahian <alahian.belaid@gmail.com>
2020-02-24 20:35:23 +0100

    sing up form

--- app/handlers.go
+++ app/handlers.go
@@ -11,6 +11,10 @@ func (a *App) HandleHome(w http.ResponseWriter, r *http.Request) {
 func (a *App) HandlePrograms(w http.ResponseWriter, r *http.Request) {
 	a.html(w, "programs.page", M{})
 }
-func (a *App)  HandleStructure(w http.ResponseWriter, r *http.Request) {
+func (a *App) HandleStructure(w http.ResponseWriter, r *http.Request) {
 	a.html(w, "structure.page", M{})
+}
+
+func (a *App) HandleApplicationForm(w http.ResponseWriter, r *http.Request) {
+	a.html(w, "sign-up.page.html", M{})
 }
