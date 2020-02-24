2e54ecf6466a958cdcc39a4b4e8cdbcc228e6bb5
yassine <agamlmameyassine@gmail.com>
2020-02-24 20:35:01 +0100

    add some validations

--- app/application_form.go
+++ app/application_form.go
@@ -11,8 +11,7 @@ import (
 type applicationForm struct {
 	fullName,
 	birthPlace,
-	job,
-	educationLevel,
+	educAndJob,
 	address,
 	phone,
 	cin,
@@ -23,15 +22,43 @@ type applicationForm struct {
 	birthDate time.Time
 }
 
-func (af *applicationForm) IsValid(errors *v.Errors)  {
-	errors := v.Validate(
+func (af *applicationForm) IsValid(errors *v.Errors)   {
+
+	err := v.Validate(
 		&vs.StringLengthInRange{
 			Name : "fullName",
 			Field : strings.TrimSpace(af.fullName),
 			Min : 6,
 			Max : 50,
-			Message : "المرجو إعادة كتابة الإسم"
-		}
+			Message : "المرجو إعادة كتابة الإسم",
+		},
+		&vs.StringLengthInRange{
+			Name : "birthPlace",
+			Field : strings.TrimSpace(af.birthPlace),
+			Min : 3,
+			Max : 30,
+			Message : "المرجو إعادة كتابة مكان لازدياد",
+		},
+		&vs.StringLengthInRange{
+			Name : "educAndJob",
+			Field : strings.TrimSpace(af.educAndJob),
+			Min : 4,
+			Max : 30,
+			Message : "المرجو إعادة كتابة المهنة/المستوى الدراسي",
+		},
+		&vs.RegexMatch{
+			Name : "phone",
+			Field : strings.TrimSpace(af.phone),
+			Expr : "^(\\+)?([0-9]){6,13}$",
+			Message : "المرجو إعادة كتابة رقم الهاتف بشكل صحيح",
+		},
+		&vs.RegexMatch{
+			Name : "cin",
+			Field : strings.TrimSpace(af.cin),
+			Expr : "^([a-z]){1,2}([0-9]{6})$",
+			Message : "المرجو إعادة كتابة رقم الهاتف بشكل صحيح",
+		},
+
 	)
-	return
+	return err 
 }
