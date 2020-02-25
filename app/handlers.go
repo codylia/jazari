99d5271af901cf57bea03df7e6222d8283f09518
usama <oussamazouaki4@gmail.com>
2020-02-25 17:43:40 +0100

    add appform post handler

--- app/handlers.go
+++ app/handlers.go
@@ -2,6 +2,9 @@ package app
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/oussama4/validate/v4"
 )
 
 func (a *App) HandleHome(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +20,34 @@ func (a *App) HandleStructure(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) HandleApplicationForm(w http.ResponseWriter, r *http.Request) {
 	a.html(w, "appform.page", M{})
+}
+
+func (a *App) HandleAppFormPost(w http.ResponseWriter, r *http.Request) {
+	bd, err := time.Parse(time.RFC3339, r.FormValue("birthDate"))
+	if err != nil {
+		a.serverError(w, err)
+		return
+	}
+	af := applicationForm{
+		fullName:     r.FormValue("fullName"),
+		birthPlace:   r.FormValue("birthPlace"),
+		educAndJob:   r.FormValue("educAndJob"),
+		address:      r.FormValue("address"),
+		phone:        r.FormValue("phone"),
+		cin:          r.FormValue("cin"),
+		email:        r.FormValue("email"),
+		tajweedLevel: r.FormValue("tajweedLevel"),
+		hifdAmount:   r.FormValue("hifdAmount"),
+		reason:       r.FormValue("reason"),
+		// imgName:      r.FormValue("imgName"),
+		birthDate: bd,
+	}
+
+	errors := validate.Validate(&af)
+	if errors.HasAny() {
+		data := M{"af": af, "errors": errors}
+		a.html(w, "appform.page", data)
+	} else {
+		http.Redirect(w, r, "/application-form/print", http.StatusOK)
+	}
 }
